62a5ec7ba0d1aaae02e8b5452caa0807e0ce0ff7
Anthony Ossent <frag132@gmail.com>
2017-01-12 22:52:04 +0100

    update travis

--- src/Dropshippers/APIBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Dropshippers/APIBundle/Tests/Controller/DefaultControllerTest.php
@@ -3,22 +3,15 @@
 namespace Dropshippers\APIBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\HttpFoundation\Response;
 
 class DefaultControllerTest extends WebTestCase
 {
-//    public function testIndex()
-//    {
-//        $client = static::createClient();
-//
-//        //$crawler = $client->request('GET', '/');
-//
-//        //$this->assertContains('Hello World', $client->getResponse()->getContent());
-//
-//        $response = new Response();
-//        $response->setStatusCode(200);
-//        $response->setContent('hacked by specthram lol');
-//
-//        $this->assertContains('Hello World', $response->getContent());
-//    }
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertContains('Hello World', $client->getResponse()->getContent());
+    }
 }
